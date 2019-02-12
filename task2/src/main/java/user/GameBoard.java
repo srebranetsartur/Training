4b38637820707f0481925f8808774b4b933dfeaf
Srebranets Artur <srebranetsartur@gmai.com>
2019-02-12 16:26:31 +0200

    Change constant signature. Add output message

--- task2/src/main/java/user/GameBoard.java
+++ task2/src/main/java/user/GameBoard.java
@@ -2,13 +2,14 @@ package user;
 
 public class GameBoard {
     //Delimiters uses for printRange
-    private final static char PREFIX = '[';
-    private final static String OVERLAPS = "...";
-    private final static char SUFFIX = ']';
+    public final static String PREFIX = "[";
+    public final static String OVERLAPS = "...";
+    public final static String SUFFIX = "]";
 
     public enum Message {
-        INPUT_RANGE("Enter two value for game range in any order"),
-        BOUND_ERROR("Wrong lower range limit. It must be in range: "),
+        INPUT_RANGE("Enter two value for game range"),
+        OK_NEXT("Enter next range"),
+        BOUND_ERROR("Wrong lower range limit. It must be in range"),
         START_GAME("Number is ready. Game started"),
         ENTER_QUIZ("Please enter your quiz"),
         ALREADY_IN("Almost correct. Take a tip. Find number in this range: "),
@@ -28,6 +29,8 @@ public class GameBoard {
         }
 
     }
+
+    public void printMessage(String message) {System.out.println(message);}
 
     public void printMessage(Message message) {
         System.out.println(message.getMessage());
